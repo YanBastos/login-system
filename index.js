e92c292e3f31380aab92209bf34e6d7e2f77519b
YanBastos <yanmoraesbastos@hotmail.com>
2023-06-14 13:47:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const port = 3000; //porta 3000
+var path = require('path'); //manipula e seta o dir das views
+const app = express(); // chamando express para definir rotas
+
+var login = "admin";
+var password = "123456";
+
+
+app.use(session({ secret: 'ausuidhaisuhaius' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+app.post('/', (req, res) => {
+
+    if (req.body.password == password && req.body.login == login) {
+        //logado com sucesso
+        req.session.login = login;
+        res.redirect('logado');
+    }else{
+        res.render('index');
+    }
+})
+
+app.get('/', (req, res) => {
+    if (req.session.login) {
+        res.render('logado');
+    } else {
+        res.render('index');
+
+    }
+})
+
+app.listen(port, () => {
+    console.log('servidor funcionando');
+}) 
